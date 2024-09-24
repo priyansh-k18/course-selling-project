f7e8e72500c4f2adc5608e262434f1589d80ab91
Priyansh Keer <153297539+priyansh-k18@users.noreply.github.com>
2024-09-24 20:02:49 +0530

    validation added

--- db.js
+++ db.js
@@ -1,35 +1,36 @@
 const mongoose = require('mongoose');
+const {z} = require('zod');
 
 const Schema = mongoose.Schema;
 const objectId = mongoose.Types.ObjectId;
 
 const userSchema = new Schema({
       userId : objectId,
-      email : String,
-      password: String,
-      name:String
+      email : z.string().email('Invalid email format'),
+      password: z.string().min(6,"password must be at least 6 characters"),
+      name: z.string().min(1,"Name is required")
 })
 
 const adminSchema = new Schema({
        adminId:objectId,
-       email: String,
-       password: String,
-       name:String
+       email : z.string().email('Invalid email format'),
+       password: z.string().min(6,"password must be at least 6 characters"),
+       name: z.string().min(1,"Name is required")
 })
 
 const courseSchema = new Schema({
        courseId:objectId,
-       title:String,
-       description:String,
-       price:Number,
-       imageUrl:String,
-       creatorId:objectId
+       title: z.string().min(1, "Title is required"),
+       description: z.string().min(1, "Description is required"),
+       price: z.number().positive("Price must be a positive number"),
+       imageUrl: z.string().url("Invalid URL format"),
+       creatorId: z.string().refine((id) => mongoose.Types.ObjectId.isValid(id), "Invalid creator ID")
 })
 
 const purchaseSchema = new Schema({
        purschaseId: objectId,
-       courseId: objectId,
-       userId: objectId
+       courseId: z.string().refine((id) => mongoose.Types.ObjectId.isValid(id), "Invalid course ID"),
+       userId: z.string().refine((id) => mongoose.Types.ObjectId.isValid(id), "Invalid user ID"),
 })
 
 const userModel = mongoose.model('user',userSchema);
